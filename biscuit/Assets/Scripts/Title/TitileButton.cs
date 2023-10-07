38f9c757a0019ea34aa80a810e35a528b4c94a26
坂本翔唯 <221093@st.yoshida-g.ac.jp>
2023-10-07 17:04:41 +0900

    タイトルコード

---
+++ biscuit/Assets/Scripts/Title/TitileButton.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitileButton : MonoBehaviour
+{
+    private bool bPIKAPIKA;          // 点滅
+    private int nPIKAPICount;        // 点滅用のカウント
+
+    //======================================
+    //  初期化処理
+    //======================================
+    void Start()
+    {
+        bPIKAPIKA = false;
+        nPIKAPICount = 0;
+    }
+
+    //======================================
+    //  更新処理
+    //======================================
+    void Update()
+    {
+        StartCoroutine(ColorChange());
+
+        if (bPIKAPIKA == true)
+        {
+            if (GetComponent<SpriteRenderer>().material.color != Color.gray)
+            {
+                GetComponent<SpriteRenderer>().material.color = Color.gray;
+            }
+        }
+        else
+        {
+            if (GetComponent<SpriteRenderer>().material.color != Color.white)
+            {
+                GetComponent<SpriteRenderer>().material.color = Color.white;
+            }
+        }
+    }
+
+    public IEnumerator ColorChange()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(1.0f);
+            bPIKAPIKA = true;
+            yield return new WaitForSeconds(1.0f);
+            bPIKAPIKA = false;
+        }
+    }
+}
--- biscuit/Assets/Scripts/Title/TitleManager.cs
+++ biscuit/Assets/Scripts/Title/TitleManager.cs
@@ -1,24 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TitleManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Application.targetFrameRate = 60;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        // Wキー（前方移動）キー入力
-        if (Input.GetKey(KeyCode.W))
+        // Enterキー（前方移動）キー入力
+        if (Input.GetKey(KeyCode.Return))
         {
             // 押された時
-            /*transform.position += 1.0f * transform.up * Time.deltaTime;*/
+            SceneManager.LoadScene("SampleScene");
         }
     }
 }
